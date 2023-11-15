39b64d82af92c16c275152872c88aeedd7310ef2
Decapitated <brettzkyjo10@gmail.com>
2023-11-14 19:23:06 -0600

    rename from spling to sampler

--- addons/poisson_disk_sampler/poisson_disk_sampler_plugin.gd
+++ addons/poisson_disk_sampler/poisson_disk_sampler_plugin.gd
@@ -5,9 +5,9 @@ var poisson_inspector_plugin: EditorInspectorPlugin
 
 func _enter_tree():
 	# Create PoissionDiskSample class type.
-	add_custom_type("PoissonDiskSampler", "Object", preload("src/poisson_disk_sampling.gd"), preload("poisson.png"))
+	add_custom_type("PoissonDiskSampler", "Object", preload("src/poisson_disk_sampler.gd"), preload("poisson.png"))
 	# Create PoissionDiskSample2D node type.
-	add_custom_type("PoissonDiskSampler2D", "Node2D", preload("src/poisson_disk_sampling_node.gd"), preload("poisson.png"))
+	add_custom_type("PoissonDiskSampler2D", "Node2D", preload("src/poisson_disk_sampler_node.gd"), preload("poisson.png"))
 	# Add custom inspector plugin
 	poisson_inspector_plugin = preload("src/poisson_inspector.gd").new()
 	add_inspector_plugin(poisson_inspector_plugin)
